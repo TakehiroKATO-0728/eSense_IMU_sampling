53fa856207585c89e8f05605187d458180542ebd
加藤　岳大 <>
2019-10-03 19:18:31 +0900

    不要な文を削除

--- app/src/main/java/jp/aoyama/a5815025/esense_imu_sampling/MainActivity.java
+++ app/src/main/java/jp/aoyama/a5815025/esense_imu_sampling/MainActivity.java
@@ -72,10 +72,8 @@ public class MainActivity extends Activity implements ESenseConnectionListener, 
         Log.d("ESENSE y",String.valueOf(val[1]));
         Log.d("ESENSE z",String.valueOf(val[2]));
         if(flag == 1){
-            //Log.d("ESENSE","start button clicked");
             progress_time = System.currentTimeMillis() - startTime;
             outputCsv.write( String.valueOf(val[0])+ "," + String.valueOf(val[1])+ "," +String.valueOf(val[2])+ "," +String.valueOf(progress_time));
-            //outputCsv.write("afjkla");
             //textView.setText("x = " + String.valueOf(val[0]) + "\n" + "y = " + String.valueOf(val[1])+ "\n" + "z = " + String.valueOf(val[2]) + "\n" + "経過時間(ミリ秒):" + String.valueOf(progress_time));
             textView.setText("計測開始");
         }
